fa4d3906300613ddba45a37f952d37be06dc0948
Bartul Bonacic <bartul.bonacic@live.com>
2017-12-11 16:00:28 +0100

    day11 parse movement directly as function

--- 2017/day11/solution.fsx
+++ 2017/day11/solution.fsx
@@ -3,24 +3,14 @@
 open System
 open System.IO
 
-type Movement = N | NE | SE | S | SW | NW
-
 let parseMovement = function
-    | "n" -> N
-    | "ne" -> NE
-    | "se" -> SE
-    | "s" -> S
-    | "sw" -> SW
-    | "nw" -> NW
+    | "n" -> fun (x, y) -> x, y + 1
+    | "ne" -> fun (x, y) -> x + 1, y
+    | "se" -> fun (x, y) -> x + 1, y - 1
+    | "s" -> fun (x, y) -> x, y - 1
+    | "sw" -> fun (x, y) -> x - 1, y
+    | "nw" -> fun (x, y) -> x - 1, y + 1
     | x -> failwithf "Unrecognized movement '%s'" x
-
-let moveNext (x, y) = function
-    | N -> x, y + 1
-    | NE -> x + 1, y
-    | SE -> x + 1, y - 1
-    | S -> x, y - 1
-    | SW -> x - 1, y
-    | NW -> x - 1, y + 1
 
 let input = 
     File.ReadAllLines(__SOURCE_DIRECTORY__ + "/input.txt") |> Array.head
@@ -37,13 +27,13 @@ let distance x y =
     let z : int = - x - y
     [x; y; z] |> List.map Math.Abs |> List.max    
 
-let x, y = data |> List.fold moveNext (0, 0)
+let x, y = data |> List.fold (fun (x, y) i -> i (x, y)) (0, 0)
 
 let result1 = distance x y
 printfn "Solution 1: %i" result1
 
 let foldWithMax (x, y, max) movement =
-    let x', y' = moveNext (x, y) movement
+    let x', y' = movement (x, y)
     let d' = distance x' y'
     let max' = if (d' > max) then d' else max;
     (x', y', max')
